eefb34deb51565fbe3688513ddadf22dd9c1b095
Felipe-Daniel <felipedaniel417n@gmail.com>
2023-06-19 13:45:47 -0300

    hello world

---
+++ src/main/java/com/in28minutes/springboot/learnspringboot/CourseController.java
@@ -0,0 +1,20 @@
+package com.in28minutes.springboot.learnspringboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourse(){
+        return Arrays.asList(
+                new Course(1,"Learn AWS","28minutes"),
+                new Course(1,"Learn AWS","28minutes")
+        );
+    }
+
+
+}
